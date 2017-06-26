b50c926b9feb9647567436c8bc8f9669b6c07950
Alex Gaynor <alex.gaynor@gmail.com>
2017-06-26 12:57:48 -0400

    Cache keys between runs to make the tests faster

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, division, unicode_literals
 
+import base64
 import datetime
 from collections import defaultdict
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 import pytest
@@ -20,9 +21,33 @@ class CertificatePair(object):
 
 
 class KeyCache(object):
-    def __init__(self):
+    def __init__(self, cached_entries):
         self._inuse_keys = defaultdict(list)
         self._free_keys = defaultdict(list)
+
+        for entry in cached_entries:
+            params = tuple(entry["params"])
+            for key in entry["keys"]:
+                key = serialization.load_der_private_key(
+                    base64.b64decode(key), password=None, backend=default_backend()
+                )
+                self._free_keys[params].append(key)
+
+    def dump(self):
+        cache_entries = []
+        for (params, keys) in self._free_keys.items():
+            cache_entries.append({
+                "params": params,
+                "keys": [
+                    base64.b64encode(key.private_bytes(
+                        serialization.Encoding.DER,
+                        serialization.PrivateFormat.PKCS8,
+                        serialization.NoEncryption()
+                    ))
+                    for key in keys
+                ],
+            })
+        return cache_entries
 
     def generate_rsa_key(self, public_exponent=65537, key_size=2048):
         params = (public_exponent, key_size)
@@ -37,6 +62,16 @@ class KeyCache(object):
         for params, keys in self._inuse_keys.items():
             self._free_keys[params].extend(keys)
         self._inuse_keys.clear()
+
+
+@pytest.fixture(scope="session")
+def key_cache(request):
+    keys = request.config.cache.get("x509-validator/keys", [])
+    key_cache = KeyCache(keys)
+    try:
+        yield key_cache
+    finally:
+        request.config.cache.set("x509-validator/keys", key_cache.dump())
 
 
 class CAWorkspace(object):
@@ -124,11 +159,6 @@ class CAWorkspace(object):
 
     def issue_new_self_signed(self):
         return self._issue_new_cert()
-
-
-@pytest.fixture(scope="session")
-def key_cache():
-    return KeyCache()
 
 
 @pytest.fixture
